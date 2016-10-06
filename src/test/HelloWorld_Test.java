8ba982a8bc3efa3a71c12ac31a3a26550d93273d
framf0rt <anders@ekendahl.one>
2016-10-06 10:54:05 +0200

    Lagt till en simpel test class

---
+++ src/test/HelloWorld_Test.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorld_Test {
+	
+	
+	@Test
+	public void mock_Pass_Test(){ // Lyckas alltid 
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void mock_Fail_Test(){ // Lyckas aldrig
+		assertEquals(2,1);
+	}
+	
+
+}
