cb0d686f752b55fd6a01f31099bdeeebf3d62ee5
framf0rt <anders@ekendahl.one>
2016-10-13 14:41:43 +0200

    Cd + cd_Test implementerat + throw exception

--- src/inte/Cd.java
+++ src/inte/Cd.java
@@ -1,11 +1,39 @@
 package inte;
 
+import java.io.File;
+
 public class Cd {
+	
+	private File currentDirectory = new File(getClass().getClassLoader().getResource("").getPath());
+	private String currentDirectoryString = "";
 	
 	public Cd(){
 		
+		
 	}
 	
+	public String cdDirectoryUpwards(){
+		File newPath = currentDirectory.getParentFile();
+		currentDirectory = newPath;
+		currentDirectoryString = newPath.getAbsolutePath();
+		return currentDirectoryString;
+		
+	}
 	
+	public String cdDirectoryChangePath(String path){ // Bör även ge feedback til användaren 
+	
+		File target = new File(path);
+		if(target.isDirectory() == true){
+			currentDirectory = target;
+			System.out.println(currentDirectory.isDirectory());
+			System.out.println(currentDirectory.isFile());
+			currentDirectoryString = currentDirectory.getAbsolutePath();
+			System.out.println(currentDirectory.toString());
+			return currentDirectoryString;
+		}
+		
+		throw new FolderDoesntExistsException("Wrong path");
+		//return currentDirectory.toString();
+	}
 
 }
---
+++ src/inte/FolderDoesntExistsException.java
@@ -0,0 +1,10 @@
+package inte;
+
+public class FolderDoesntExistsException extends RuntimeException{
+	
+	public FolderDoesntExistsException (String message){
+		super(message);
+	}
+	
+
+}
