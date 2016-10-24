0a8f15b6d789243d490d3e0abcd63b19928a5aea
AlexanderLukas <mr.vendet@gmail.com>
2016-10-24 14:51:20 +0200

    Vi har ingen aning om varför detta funkar

--- src/TestaInte/SmallDirectory.java
+++ src/TestaInte/SmallDirectory.java
@@ -1,16 +1,16 @@
 package TestaInte;
 
 public class SmallDirectory extends Directory {
-//	private static final FSO[] content = new FSO[3];
+	private static final FSO[] content = new FSO[3];
 	public SmallDirectory(String name) {
 		super(name);
-//		content[0] = new EmptyDirectory("Directory1");
-//		content[1] = new FakeFile("File1",50);
-//		content[2] = new FakeFile("File2", 100);
+		content[0] = new EmptyDirectory("Directory1");
+		content[1] = new FakeFile("File1",50);
+		content[2] = new FakeFile("File2", 100);
 		
 	}
 
-	private static final FSO[] content = {new EmptyDirectory("Directory1"), new FakeFile("File1",50), new FakeFile("File2", 100)};
+//	private static final FSO[] content = {new EmptyDirectory("Directory1"), new FakeFile("File1",50), new FakeFile("File2", 100)};
 	
 	@Override
 	public FSO[] getContent() {
