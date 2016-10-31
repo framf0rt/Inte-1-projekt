0c4b57845b0fb7d07a00b1bd07086af30a36ac89
framf0rt <anders@ekendahl.one>
2016-10-31 13:44:18 +0100

    Fixa main

---
+++ src/test/CommandTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import cmd.Command;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandTest {
+
+	private Command c;
+	private String testString;
+
+	@Before
+	public void beforeTest() {
+		c = new Command();
+	}
+
+	@Test
+	public void normalizeNullTest() {
+
+		testString = c.normalize(null);
+		assertTrue(testString.equals(""));
+	}
+
+	@Test
+	public void normalizeTest() {
+
+		testString = c.normalize("     Hej hOpP     ");
+		assertTrue(testString.equals("hej hopp"));
+	}
+
+	@Test
+	public void splitCommandTest() {
+
+		testString = c.normalize("     Hej 	  		        hOpP     ");
+		ArrayList<String> command = c.splitCommand(testString);
+		ArrayList<String> correctList = new ArrayList<>();
+		correctList.add("hej");
+		correctList.add("hopp");
+		assertEquals(correctList, command);
+
+	}
+}
